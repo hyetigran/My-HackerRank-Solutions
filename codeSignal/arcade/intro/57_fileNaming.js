4236d242ac1f5bd791ecf722f65e299ba3e9f767
Tigran Asriyan <tigran.asriyan@gmail.com>
2020-09-17 23:53:49 -0400

    file naming solution

---
+++ codeSignal/arcade/intro/57_fileNaming.js
@@ -0,0 +1,11 @@
+const fileNaming = (names) => {
+  const used = {};
+  return names.map((name) => {
+    let newName = name;
+    while (used[newName]) {
+      newName = `${name}(${used[name]++})`;
+    }
+    used[newName] = 1;
+    return newName;
+  });
+};
